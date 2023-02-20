5f50f434c1743ffedb2a3b1db10fe7efa40a9b0d
laughing.s <laughin.s@blockcustody.com>
2023-02-20 16:38:46 +0800

    .gitignore is now working

--- Example/Pods/Target Support Files/Keychainer/Keychainer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Keychainer : NSObject
-@end
-@implementation PodsDummy_Keychainer
-@end
--- Example/Pods/Target Support Files/Pods-Keychainer_Example/Pods-Keychainer_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Keychainer_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Keychainer_Example
-@end
